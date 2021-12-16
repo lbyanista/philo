c46b50fb7f7da85a304b1c0fbf16fc12a45f7ac9
lbyanista <mlabrayj@student.1337.ma>
2021-12-16 18:26:51 +0100

    noeditthen

--- check_args.c
+++ check_args.c
@@ -6,28 +6,28 @@
 /*   By: mlabrayj <mlabrayj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:31:34 by mlabrayj          #+#    #+#             */
-/*   Updated: 2021/12/16 14:48:43 by mlabrayj         ###   ########.fr       */
+/*   Updated: 2021/12/16 16:45:04 by mlabrayj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int check_args(char **av)
+int	check_args(char **av)
 {
-    int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (av[i])
 	{
 		j = 0;
 		while (av[i][j])
-        {
+		{
 			if (!ft_isdigit(av[i][j]))
 				return (1);
-            j++;
-        }
-        i++;
+			j++;
+		}
+		i++;
 	}
 	return (0);
 }
--- philo.c
+++ philo.c
@@ -6,55 +6,40 @@
 /*   By: mlabrayj <mlabrayj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 17:19:55 by mlabrayj          #+#    #+#             */
-/*   Updated: 2021/12/16 14:53:15 by mlabrayj         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:35:03 by mlabrayj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    pthread_t *philo;
-    pthread_mutex_t *mutex;
-    t_philo *p;
-    // t_time *time;
-    // void *r;
-    if (ac != 5 && ac != 6)
-    {
-        printf("waa ghi zid wla n9es args baliz\n");
-        return (1);
-    }
-    if (check_args(av + 1))
-    {
-        printf("dkhel valid numbers bruuh :(\n");
-        return (1);
-    }
-    // time = malloc(sizeof(t_time) * 200);
-    // time->time_to_die = ft_atoi(av[2]);
-    // printf("--time to reat %d--\n", time->time_to_eat);
-    // time->time_to_eat = ft_atoi(av[3]);
-    // time->time_to_sleep = ft_atoi(av[4]);
-    // time->time_must_eat = ft_atoi(av[5]);
-    // int n_philo = ft_atoi(av[1]);
-    // printf("%d\n\n", n_philo);
-    p = malloc(sizeof(t_philo) * ft_atoi(av[1]));
-    // r = malloc(sizeof(t_ret) * 3);
-    philo = malloc(sizeof(pthread_t) * ft_atoi(av[1]));
-    mutex = malloc(sizeof(pthread_mutex_t) * ft_atoi(av[1]));
-    int i = 0;
-    while(i < ft_atoi(av[1]))
-        pthread_mutex_init(&mutex[i++], NULL);
-    i = 0;
-    while(i < ft_atoi(av[1]))
-    {
-        p[i].id = i;
-        p[i++].mutex = mutex;
-    }
-    i = -1;
-    while(++i < ft_atoi(av[1]))
-        pthread_create(&philo[i], NULL, misa, &p[i]);
-    i = 0;
-    while(i < ft_atoi(av[1]))
-        pthread_join(philo[i++], NULL);
-    return 0;
+	pthread_t		*philo;
+	pthread_mutex_t	*mutex;
+	t_philo			*p;
+	int				i;
+
+	if (ac != 5 && ac != 6)
+		return (printf("waa ghi zid wla n9es args baliz\n"));
+	if (check_args(av + 1))
+		return (printf("dkhel valid numbers bruuh :(\n"));
+	p = malloc(sizeof(t_philo) * ft_atoi(av[1]));
+	philo = malloc(sizeof(pthread_t) * ft_atoi(av[1]));
+	mutex = malloc(sizeof(pthread_mutex_t) * ft_atoi(av[1]));
+	i = 0;
+	while (i < ft_atoi(av[1]))
+		pthread_mutex_init(&mutex[i++], NULL);
+	i = 0;
+	while (i < ft_atoi(av[1]))
+	{
+		p[i].id = i;
+		p[i++].mutex = mutex;
+	}
+	i = -1;
+	while (++i < ft_atoi(av[1]))
+		pthread_create(&philo[i], NULL, misa, &p[i]);
+	i = 0;
+	while (i < ft_atoi(av[1]))
+		pthread_join(philo[i++], NULL);
+	return (0);
 }
